a5e7bdb5034444d5057b63b8bc372e41d4abac4e
Tanuka Das <das.tanuka@yahoo.com>
2019-03-23 14:00:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // write your code below!
+function happyHolidays(){
+  return  "Happy holidays!"
+}
+
+function happyHolidaysTo("Tanuka"){
+  return `Happy holidays, ${name}!`
+
+}
+function happyHolidayTo(Thanksgiving, Tanuka){
+  return `Happy ${holiday}, ${name}!`
+}
+function holidayCountdown(Thanksgiving, 5){
+  return `It's ${days} days until ${holiday}!`
+}
